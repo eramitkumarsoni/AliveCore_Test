f00298ca85c3f246b70737a92ed4f8316880479e
AMIT KUMAR SONI <amit.soni@dxc.com>
2020-10-25 17:55:32 +0530

    Initial Commit

---
+++ AliveCore/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AliveCore
+//
+//  Created by Amit soni on 25/10/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
